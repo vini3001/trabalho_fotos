36f32b8613dd18330ab29b29ca069c61ad93fea2
Vinícius Donizeti <donizetevinicius250@gmail.com>
2024-05-14 17:50:38 -0300

    Adicionar arquivos de projeto.

---
+++ trabalho_fotos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace trabalho_fotos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
